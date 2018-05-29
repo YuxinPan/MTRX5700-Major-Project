2db0175594d95466717c7576513bab886fb28e78
Daniel Sun <Daniel.y.sun@icloud.com>
2018-05-29 20:31:16 +1000

    Minor adjustments, played around with ICP point cloud.

--- kinectSandbox/KinectPt.m
+++ kinectSandbox/KinectPt.m
@@ -1,9 +1,13 @@
-% release(colorDevice);
-% release(depthDevice);
+%% If we haven't released the devices, then release them
+exist colorDevice;
+if ans
+    release(colorDevice);
+    release(depthDevice);
+end
 
+
+%% Lets connect to the devices
 colorDevice = imaq.VideoDevice('kinect',1)
-
-
 depthDevice = imaq.VideoDevice('kinect',2)
 
 
@@ -17,45 +21,50 @@ depthImage = step(depthDevice);
 ptCloud = pcfromkinect(depthDevice,depthImage,colorImage);
 
 player = pcplayer(ptCloud.XLimits,ptCloud.YLimits,ptCloud.ZLimits,...
-	'VerticalAxis','y','VerticalAxisDir','down');
+    'VerticalAxis','y','VerticalAxisDir','down');
 
 xlabel(player.Axes,'X (m)');
 ylabel(player.Axes,'Y (m)');
 zlabel(player.Axes,'Z (m)');
 
-
-for i = 1:500    
-   colorImage = step(colorDevice);  
-   depthImage = step(depthDevice);
- 
-   ptCloud = pcfromkinect(depthDevice,depthImage);
-   x = reshape(ptCloud.Location(:,:,1),[],1)';
-   y = reshape(ptCloud.Location(:,:,2),[],1)';
-   z = reshape(ptCloud.Location(:,:,3),[],1)';
-   xyz = [x; y; z];
-   k = find(ptCloud.Location(:,:,2) > -0.30); 
-   xyz = xyz(:,k);
-   k = find(xyz(2,:) < -0.10); 
-   xyz = xyz(:,k);
-   
-   view(player,ptCloud);
-%    figure(1);
-   hold off;
-   grid on;
-   plot(xyz(1,:), xyz(3,:),'.');
-   xlabel('x');
-   ylabel('y');
-%    zlabel('z');
+% Show 500 images
+for i = 1:500
+    colorImage = step(colorDevice);
+    depthImage = step(depthDevice);
+    
+    ptCloud = pcfromkinect(depthDevice,depthImage);
+    x = reshape(ptCloud.Location(:,:,1),[],1)';
+    y = reshape(ptCloud.Location(:,:,2),[],1)';
+    z = reshape(ptCloud.Location(:,:,3),[],1)';
+    xyz = [x; y; z];
+    k = find(ptCloud.Location(:,:,2) > -0.30);
+    xyz = xyz(:,k);
+    k = find(xyz(2,:) < -0.15);
+    xyz = xyz(:,k);
+    
+    view(player,ptCloud);
+    figure(1);
+    hold off;
+    grid on;
+    plot(xyz(1,:), xyz(3,:),'.');
+    xlabel('x');
+    ylabel('y');
+    %    zlabel('z');
     axis equal;
     axis([-5 5 0 9]);
     
-    pause(1);
-%    figure(2);
-%    subplot(1,2,1);
-%    imshow(depthImage);
-%    subplot(1,2,2);
-%    imshow(colorImage);
+    
+    %    figure(2);
+    %    subplot(1,2,1);
+    %    
+
+%     figure(2);
+%     subplot(1,2,1);
+%     imshow(flipdim(depthImage.*10,2));
+%     subplot(1,2,2);
+%     imshow(flipdim(colorImage, 2));
+% %     pause(1);
 end
-%%
+%% Release the devices
 release(colorDevice);
 release(depthDevice);
